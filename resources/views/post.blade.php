eab1066130b0c6468672fa09956880269a6e3832
uoslau <nelsonsamosir@gmail.com>
2023-12-16 16:34:17 +0700

    laravel-5

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+    <article>
+        <h2>{{ $post["title"] }}</h2>
+        <p>{{ $post["body"] }}</p>
+    </article>
+
+    <a href="/posts">Back to Posts</a>
+@endsection
