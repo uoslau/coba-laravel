76443de9a2a7a46ef533607d13ea8965073ac49f
uoslau <nelsonsamosir@gmail.com>
2023-12-28 18:29:51 +0700

    laravel 9 (mungkin)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,48 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, recusandae.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis excepturi quia. Distinctio quod minus tempora animi. Consectetur beatae at, deleniti non repellendus ab dicta rerum sunt, vero magni culpa quia blanditiis exercitationem nihil. Voluptatum voluptates id totam iste, veniam iure eum dolore similique sequi odit aliquam maxime ex voluptatibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, recusandae.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis excepturi quia. Distinctio quod minus tempora animi. Consectetur beatae at, deleniti non repellendus ab dicta rerum sunt, vero magni culpa quia blanditiis exercitationem nihil. Voluptatum voluptates id totam iste, veniam iure eum dolore similique sequi odit aliquam maxime ex voluptatibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, recusandae.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis excepturi quia. Distinctio quod minus tempora animi. Consectetur beatae at, deleniti non repellendus ab dicta rerum sunt, vero magni culpa quia blanditiis exercitationem nihil. Voluptatum voluptates id totam iste, veniam iure eum dolore similique sequi odit aliquam maxime ex voluptatibus?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
     }
 }
