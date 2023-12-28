97e6cb81527b82a3dc399a38a468acbc73bfa68b
uoslau <nelsonsamosir@gmail.com>
2023-12-28 10:31:35 +0700

    laravel-8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
     <h1 class="mb-5">{{ $post->title }}</h1>
 
+    <p>By. John Doe in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
 
